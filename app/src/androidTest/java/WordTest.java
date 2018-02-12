60d7872b65726feca301ba08d9ddd812b862c3ed
toroden79 <toroden@gmail.com>
2018-02-11 22:12:03 -0600

    Initial Commit - added content to the 2 build.gradle files as instructed.

---
+++ app/src/androidTest/java/WordTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bear6 on 2/11/2018.
+ */
+public class WordTest {
+    @Test
+    public void getWord() throws Exception {
+    }
+
+}
---
+++ app/src/main/java/Word.java
@@ -0,0 +1,21 @@
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by bear6 on 2/11/2018.
+ */
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(String word) {this.mWord = word;}
+
+    public String getWord(){return this.mWord;}
+}
