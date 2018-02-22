365a9bc8473652824499feb10193d2a73e3acbb8
toroden79 <toroden@gmail.com>
2018-02-22 13:11:04 -0600

    Step 9 - Create the ViewModel

---
+++ app/src/main/java/com/example/bear6/roomwordsample/WordViewModel.java
@@ -0,0 +1,28 @@
+package com.example.bear6.roomwordsample;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import java.util.List;
+
+/**
+ * Created by bear6 on 2/22/2018.
+ */
+
+public class WordViewModel extends AndroidViewModel {
+
+    private WordRepository mRepository;
+
+    private LiveData<List<Word>> mAllWords;
+
+    public WordViewModel (Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() { return mAllWords; }
+
+    public void insert(Word word) { mRepository.insert(word); }
+}
